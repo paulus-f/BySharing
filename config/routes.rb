c1c9f6c3f7125f410926f32f7073702cce2f8139
Pavel <fedorovich2012bel@gmail.com>
2019-06-01 14:23:59 +0300

    return json response in auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root 'root#index'
   namespace :api do
     namespace :v1 do
       get '/index', to: 'home#index'
     end
   end
-
-  root 'root#index'
 end
