28ea12b89ccb4e8a08b959039ef70d2868f49ecd
Pavel <fedorovich2012bel@gmail.com>
2019-06-02 12:22:37 +0300

    fix auth

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  #skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   respond_to :json
 
